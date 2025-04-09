f154843f80a7d612106a7b2df83f190affa47ab8
Timothée Sanchez <timothee.sanchez.faly@gmail.com>
2025-04-09 15:33:56 +0200

    Added clean pipeline for MRI preprocessing
    - Class definition for subject specific pipeline

---
+++ cerebellum_folding/data/preprocess.py
@@ -0,0 +1,205 @@
+"""Library for the preprocessing pipeline"""
+
+
+from cerebellum_folding.data.path import SubjectPath, MaskPath, check_file
+
+from deep_folding.brainvisa.utils.resample import resample
+
+from typing import List, Dict, Union
+from pathlib import Path
+from soma import aims
+import numpy as np
+
+
+class PipelineSubject :
+    def __init__(self,
+                 subject_path : SubjectPath,
+                 masks_path : Dict[str, MaskPath], 
+                 white_matter_threshold : float, 
+                 sulci_threshold : float, 
+                 resample_values_icbm : List[int], 
+                 resample_values_bin : List[int],
+                 output_voxel : List[int], 
+                 verbose : bool = False,
+                ):
+        # Retrieving paths of the subject
+        self.path = subject_path
+        self.id = self.path.id
+
+        # Creating saving folders
+        self.path.create_saving_paths()
+
+        self.masks_path = masks_path
+        
+        # Parameters of the pipeline
+        self.wm_thresh = white_matter_threshold
+        self.sulci_thresh = sulci_threshold
+        self.resample_val_icbm = resample_values_icbm
+        self.resample_val_bin = resample_values_bin
+        self.resample_output_voxel = output_voxel
+
+        self.verbose = verbose
+    
+    def print(self, string):
+        if self.verbose : 
+            print(string)
+
+
+    def threshold_mean_curvature(self,
+                                overwrite : bool = False,
+                                ):
+        
+        if not check_file(self.path.mc):
+            raise Exception(f"Mean curvature file not available : {self.path.mc}")
+        
+        if check_file(self.path.native["threshold"]):
+            if not overwrite : 
+                raise Exception("File already exists")
+            else : 
+               self.print(f"Overwriting : {self.path.native['threshold']}") 
+
+        white_matter_thresh = self.wm_thresh
+        sulci_thresh = self.sulci_thresh
+
+        obj = aims.read(str(self.path.mc))
+        vol = obj.np
+
+        # Mask for WM and Sulci
+        white_matter_mask = (vol <= white_matter_thresh) 
+        sulci_mask = (vol >= sulci_thresh)
+        rest = (vol > white_matter_thresh) & (vol < sulci_thresh) 
+
+        #Apply Mask : 
+        vol[white_matter_mask] = -1
+        vol[sulci_mask] = 1
+        vol[rest] = 0
+
+        # Convert from float type to S16 more convinient
+        conv = aims.Converter(intype=obj, outtype=aims.Volume("S16")) 
+        vol = conv(obj)
+
+        self.print(f"Saving to {self.path.native['threshold']}")
+        aims.write(obj, filename=str(self.path.native['threshold']))
+
+
+    def get_binary_val(self,
+                      overwrite : bool = False,
+                    ):
+
+        if not check_file(self.path.native["threshold"]):
+            raise Exception("Threshold path doesn't exist")
+
+
+        for to_isolate in ["sulci", "white_matter"]:
+            
+            if check_file(self.path.native[to_isolate]):
+                if not overwrite : 
+                    raise Exception("File already exists")
+                else : 
+                    self.print(f"Overwriting : {self.path.native[to_isolate]} ") 
+
+            # Value corresponding the the encoding in the MC Thresholded
+            val = 1 if to_isolate == "sulci" else -1
+            saving_path = self.path.native[to_isolate]
+
+            thresh_vol = aims.read(str(self.path.native["threshold"]))
+            thresh_np = thresh_vol.np
+
+            # Retrieving masks for the analysis
+            mask = (thresh_np == val)
+
+            thresh_np[mask] = 1
+            thresh_np[~mask] = 0
+
+            self.print(f"Saving {saving_path}")
+            aims.write(thresh_vol, str(saving_path))
+    
+    def _get_transform_mat(self):
+        if not self.path.transform_mat :
+            graph = aims.read(str(self.path.graph))
+            transf = aims.GraphManip.getICBM2009cTemplateTransform(graph)
+            return transf
+        else :
+            print("Transformation matrix available")
+            return None
+    
+    
+    def _apply_ICBM2009c(self,
+                         source_path : Path,
+                         saving_path : Path,
+                         resample_values : List[int],
+                         ):
+
+        # Loading object
+        native_obj = aims.read(str(source_path))
+        mat_transform = self._get_transform_mat()
+
+        #Convert for sanity
+        c = aims.Converter(intype=native_obj, outtype=aims.Volume('S16'))
+        native_obj = c(native_obj)
+
+        resampled_to_ICBM2009c = resample(
+            input_image=native_obj, 
+            transformation=mat_transform,
+            output_vs=self.resample_output_voxel,
+            background=0,
+            values=resample_values,
+            verbose=self.verbose
+        )
+
+        self.print(f"Saving {saving_path}")
+        aims.write(resampled_to_ICBM2009c, filename=str(saving_path))
+
+    def transform_ICBM2009c(self, overwrite : bool = False):
+
+        for type_file in ["threshold", "sulci", "white_matter"]:
+
+            resample_values = self.resample_val_bin if type_file in ["sulci", "white_matter"] else self.resample_val_icbm
+
+            if check_file(self.path.icbm[type_file]):
+                if not overwrite : 
+                    raise Exception("File already exists")
+                else : 
+                    self.print(f"Overwriting : {self.path.icbm[type_file]} ") 
+
+            self._apply_ICBM2009c(source_path= self.path.native[type_file],
+                                  saving_path=self.path.icbm[type_file],
+                                  resample_values=resample_values)
+
+    def _apply_mask(self,
+                    source_path : Path,
+                    saving_path : Path,
+                    mask_path : Path,
+                    ): 
+        
+        # Reading objects
+        obj = aims.read(str(source_path))
+        mask = aims.read(str(mask_path))
+
+        # Convert mask to bool
+        mask_bool = np.where(mask.np == 1, True, False)
+
+        # Apply mask
+        obj.np[~mask_bool] = 0
+
+        self.print(f"Saving {saving_path}")
+        aims.write(obj, filename=str(saving_path))
+    
+    def apply_masks(self, 
+                    overwrite : bool = False):
+        for type_mask, mask_path in self.masks_path.items(): 
+
+            path_mask = mask_path.icbm2009
+
+            for type_file in ["threshold", "sulci", "white_matter"]:
+
+                if check_file(self.path.masked[type_mask][type_file]):
+                    if not overwrite : 
+                        raise Exception("File already exists")
+                    else : 
+                        self.print(f"Overwriting : {self.path.masked[type_mask][type_file]} ") 
+                self._apply_mask(
+                    source_path=self.path.icbm[type_file],
+                    saving_path=self.path.masked[type_mask][type_file],
+                    mask_path= path_mask
+                )
