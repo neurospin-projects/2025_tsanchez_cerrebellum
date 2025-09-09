b5769d2a5082b3068b8f16be9311f76519687608
Timothée Sanchez <timothee.sanchez.faly@gmail.com>
2025-09-09 15:15:38 +0200

    [UPDATE] Modified embedding generation
    Splitted generate_emb function to be able to produce only mu values or all the previous outputs that the function generated (mu, logvar, reconstruction, input,...)

--- betaVAE/compute_emb.py
+++ betaVAE/compute_emb.py
@@ -9,7 +9,7 @@ import numpy as np
 saveable = lambda x : x.cpu().detach().numpy()
 
 
-def generate_emb(data_path : Path, exp_path : Path, to_load :str, device : str, dataset_name : str) : 
+def generate_output(data_path : Path, exp_path : Path, to_load :str, device : str, dataset_name : str) : 
     results = dict()
     mu_s, logvar_s, recons_s, input_s, subject_s = [], [], [], [], []
 
@@ -40,11 +40,45 @@ def generate_emb(data_path : Path, exp_path : Path, to_load :str, device : str, 
 
     return results
 
+
+def compute_emb_only(data_path : Path, exp_path : Path, to_load :str, device : str, dataset_name : str) : 
+    results = dict()
+    mu_s, logvar_s, subject_s = [], [], []
+
+    model = load_trained_model(exp_path=exp_path, to_load= to_load, device=device)
+    model.to(device)
+
+    dataset = load_dataset(data_path=data_path, exp_path=exp_path, dataset_name=dataset_name)
+    loader = DataLoader(dataset=dataset, batch_size=1)
+
+    model.eval()
+    for split, full_vol, subject in tqdm(loader) : 
+        with torch.no_grad():
+            wm_vol = split[:,0,:,:,:].unsqueeze(1)
+            inputs = Variable(wm_vol).to(device, dtype = torch.float32)
+            logits, mu, logvar = model(inputs)
+            subject_s.append(subject[0]) # ! batch size == 1
+            mu_s.append(saveable(mu))
+            logvar_s.append(saveable(logvar))
+
+    results["id_sub"] = np.array(subject_s)
+    results["mu"] = np.array(mu_s)
+
+    return results
+
+
+def generate_emb(data_path : Path, exp_path : Path, to_load :str, device : str, dataset_name : str, only_emb : bool = False) :
+    if only_emb : 
+        results = compute_emb_only(data_path, exp_path, to_load, device, dataset_name)
+    else : 
+        results = generate_output(data_path, exp_path, to_load, device, dataset_name) 
+    return results
+
 if __name__ == "__main__": 
     EXP_PATH = Path("/neurospin/dico/tsanchez/Jean_zay_runs/08_model_choice/2025-06-14/08-13-28_14878")
     DATA_PATH = Path("/neurospin/dico/tsanchez/preprocessed/UKBio1000")
 
-    results = generate_emb(data_path=DATA_PATH,
+    results = generate_output(data_path=DATA_PATH,
                         exp_path= EXP_PATH, 
                         to_load= "checkpoint",
                         device="cuda",
